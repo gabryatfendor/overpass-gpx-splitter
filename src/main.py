e0552c484eecf856158705892d6256fcfec4a4b0
gabryatfendor <sani.gabriele@protonmail.com>
2022-03-29 17:49:52 +0200

    Added regex to create gpx with useful names

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import gpxpy
 import gpxpy.gpx
 import os
+import regex
 
 gpx_file = open('res/sample.gpx', 'r')
 
 gpx_input = gpxpy.parse(gpx_file)
 
-for i,track in enumerate(gpx_input.tracks):
+for track in gpx_input.tracks:
     gpx_output = gpxpy.gpx.GPX()
     gpx_output.tracks.append(track)
-    with open(os.path.dirname(__file__) + "/output/" + str(i) + ".gpx", "w") as f:
+    result = regex.search(r'ref=\K(.*)', gpx_output.to_xml()) 
+    with open(os.path.dirname(__file__) + "/output/" + str(result[0]) + ".gpx", "w") as f:
         f.write(gpx_output.to_xml())
